657132c707c0f0f34fe6cce3330f25afeab93416
lonesomegeek <lonesomegeek@outlook.com>
2017-05-23 19:50:31 -0400

    base crud controller with getall and getbyid

---
+++ LSG.GenericCrud/DAL/Crud.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace LSG.GenericCrud.DAL
+{
+    public class Crud<T> : ICrud<T>
+        where T : class, IEntity, new()
+    {
+        private readonly IDbContext _context;
+
+        public Crud(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> GetAll() => _context.Set<T>().AsEnumerable();
+
+        public T GetById(Guid id) => _context.Set<T>().SingleOrDefault(_ => _.Id == id);
+    }
+
+    public interface ICrud<TDto, TEntity>
+    {
+        IEnumerable<TDto> GetAll();
+        TDto GetById(Guid id);
+        TDto Create(TDto dto);
+        void Update(Guid id, TDto dto);
+        void Delete(Guid id);
+    }
+
+    public interface ICrud<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Guid id);
+        //T Create(T entity);
+        //void Update(Guid id, T entity);
+        //void Delete(Guid id);
+    }
+}
