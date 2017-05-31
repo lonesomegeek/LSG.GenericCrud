58fdc5f52d5980cb1f0b0ee0ef9c4f82ae60fe06
lonesomegeek <lonesomegeek@outlook.com>
2017-05-30 20:31:02 -0400

    minimal working version of middleware validating token scopes

---
+++ LSG.GenericCrud/Middlewares/AuthorizationMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LSG.GenericCrud.Middlewares
+{
+    public class AuthorizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthorizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var scopes = context.User.Claims.FirstOrDefault(_ => _.Type == "scope")?.Value.Split(' ');
+
+            var ressourceRequested = context.Request.Path.Value.Split('/')[2];
+            var roleRequired = context.Request.Method == "GET" ? "read" : "manage";
+            var scopeRequired = string.Format("{0}:{1}", roleRequired, ressourceRequested);
+            if (scopes.Count(_ => _ == scopeRequired) >= 1)
+            {
+                await _next.Invoke(context);
+            }
+            else
+            {
+                context.Response.StatusCode = (int) HttpStatusCode.Forbidden;
+                await context.Response.WriteAsync("Missing scope");
+                return;
+            }
+
+        }
+    }
+}
