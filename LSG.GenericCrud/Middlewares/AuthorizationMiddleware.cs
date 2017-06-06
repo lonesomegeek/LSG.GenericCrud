ca1171c42182c704da95fd3db048ad6d17a8614f
lonesomegeek <lonesomegeek@outlook.com>
2017-06-06 12:31:50 -0400

    working version, ultra restrained! one scope per policy needed

--- LSG.GenericCrud/Middlewares/AuthorizationMiddleware.cs
+++ LSG.GenericCrud/Middlewares/AuthorizationMiddleware.cs
@@ -19,21 +19,42 @@ namespace LSG.GenericCrud.Middlewares
         public async Task Invoke(HttpContext context)
         {
             var scopes = context.User.Claims.FirstOrDefault(_ => _.Type == "scope")?.Value.Split(' ');
+            
+            //if (scopes == null)
+            //{
+            //    ForbiddenAccess(context);
+            //    return;
+            //}
 
+            //// is god!
+            //if (scopes.Count(_ => _ == "all:all") == 1) await _next.Invoke(context);
+            //// is managing the resource?
+            //else if (scopes.Count(_ => _ == $"all:{context.Request.Path.Value.Split('/')[2]}") == 1) await _next.Invoke(context);
+            //// have the required scope for the ressource
+            //else if (scopes.Count(_ => _ == GetRequiredScope(context)) >= 1) await _next.Invoke(context);
+            //// else... no access
+            //else ForbiddenAccess(context);
+
+            if (scopes != null && scopes.Count(_ => _ == GetRequiredScope(context)) >= 1) await _next.Invoke(context);
+            else ForbiddenAccess(context);
+
+        }
+
+        private async void ForbiddenAccess(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            await context.Response.WriteAsync("Access to the ressource is forbidden.");
+        }
+
+        private string GetRequiredScope(HttpContext context)
+        {
+            var method = context.Request.Method;
             var ressourceRequested = context.Request.Path.Value.Split('/')[2];
-            var roleRequired = context.Request.Method == "GET" ? "read" : "manage";
-            var scopeRequired = string.Format("{0}:{1}", roleRequired, ressourceRequested);
-            if (scopes.Count(_ => _ == scopeRequired) >= 1)
-            {
-                await _next.Invoke(context);
-            }
-            else
-            {
-                context.Response.StatusCode = (int) HttpStatusCode.Forbidden;
-                await context.Response.WriteAsync("Missing scope");
-                return;
-            }
-
+            if (HttpMethods.IsGet(method)) return $"read:{ressourceRequested}";
+            if (HttpMethods.IsPost(method)) return $"create:{ressourceRequested}";
+            if (HttpMethods.IsPut(method)) return $"update:{ressourceRequested}";
+            if (HttpMethods.IsDelete(method)) return $"delete:{ressourceRequested}";
+            return String.Empty;
         }
     }
 }
