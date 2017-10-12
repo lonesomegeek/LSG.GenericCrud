6cd9ef074ff7d787be09d2cb2deb2d9930e40cf1
lonesomegeek <lonesomegeek@outlook.com>
2017-10-11 20:26:46 -0400

    adding new service tests

---
+++ LSG.GenericCrud/Repositories/ICrudRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LSG.GenericCrud.Repositories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ICrudRepository<T>
+    {
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// Gets the by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        T GetById(Guid id);
+        Task<T> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Creates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        T Create(T entity);
+        Task<T> CreateAsync(T entity);
+
+        /// <summary>
+        /// Updates the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="entity">The entity.</param>
+        void Update(Guid id, T entity);
+
+        Task UpdateAsync(Guid id, T entity);
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        void Delete(Guid id);
+
+        Task DeleteAsync(Guid id);
+
+
+    }
+}
---
+++ LSG.GenericCrud/Services/CrudService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using LSG.GenericCrud.Exceptions;
+using LSG.GenericCrud.Models;
+using LSG.GenericCrud.Repositories;
+
+namespace LSG.GenericCrud.Services
+{
+    public class CrudService<T> : ICrudService<T>
+    {
+        private readonly ICrudRepository<T> _repository;
+
+        public CrudService(ICrudRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<T> GetAll() => _repository.GetAll();
+
+        public T GetById(Guid id)
+        {
+            var entity = _repository.GetById(id);
+            if (entity == null) throw new EntityNotFoundException();
+            return entity;
+        }
+
+        public T Create(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
