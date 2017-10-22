51e13d97ccc610a569c458c723ef482a79809cfe
lonesomegeek <lonesomegeek@outlook.com>
2017-10-22 12:10:55 -0400

    little cleanup + add crud service dto

---
+++ LSG.GenericCrud.Dto/Services/CrudService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using LSG.GenericCrud.Models;
+using LSG.GenericCrud.Repositories;
+using LSG.GenericCrud.Services;
+
+namespace LSG.GenericCrud.Dto.Services
+{
+    public class CrudService<TDto, TEntity> : CrudService<TEntity> 
+        where TDto : IEntity 
+        where TEntity : IEntity
+    {
+        private readonly IMapper _mapper;
+
+        public CrudService(ICrudRepository<TEntity> repository, IMapper mapper) : base(repository)
+        {
+            _mapper = mapper;
+        }
+
+        public TDto Create(TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+            var createdEntity = base.Create(entity);
+            return _mapper.Map<TDto>(createdEntity);
+        }
+    }
+}
--- LSG.GenericCrud.Tests/Controllers/CrudDtoControllerTestsWithoutService.cs
+++ LSG.GenericCrud.Tests/Controllers/CrudDtoControllerTestsWithoutService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using Bogus;
 using LSG.GenericCrud.Controllers;
+using LSG.GenericCrud.Dto.Controllers;
 using LSG.GenericCrud.Exceptions;
 using LSG.GenericCrud.Repositories;
 using LSG.GenericCrud.Tests.Models;
@@ -13,12 +15,15 @@ using Xunit;
 namespace LSG.GenericCrud.Tests.Controllers
 {
     
-    //public class CrudControllerWithoutServiceTests
+    //public class CrudDtoControllerTestsWithoutService
     //{
     //    private readonly IList<TestEntity> _entities;
     //    private readonly TestEntity _entity;
+    //    private readonly IMapper _mapper;
+    //    private readonly IList<TestDto> _dtos;
+    //    private readonly TestDto _dto;
 
-    //    public CrudControllerWithoutServiceTests()
+    //    public CrudDtoControllerTestsWithoutService()
     //    {
     //        Randomizer.Seed = new Random(1234567);
     //        var entityFaker = new Faker<TestEntity>().
@@ -26,6 +31,19 @@ namespace LSG.GenericCrud.Tests.Controllers
     //            RuleFor(_ => _.Value, _ => _.Lorem.Word());
     //        _entities = entityFaker.Generate(5);
     //        _entity = entityFaker.Generate();
+
+    //        var dtoFaker = new Faker<TestDto>().
+    //            RuleFor(_ => _.Id, Guid.NewGuid()).
+    //            RuleFor(_ => _.ValueDto, _ => _.Lorem.Word());
+    //        _dtos = dtoFaker.Generate(5);
+    //        _dto = dtoFaker.Generate();
+
+    //        _mapper = new AutoMapper.MapperConfiguration(_ =>
+    //        {
+    //            _.CreateMap<TestDto, TestEntity>().ForMember(dest => dest.Value, opts => opts.MapFrom(src => src.ValueDto));
+    //            _.CreateMap<TestEntity, TestDto> ().ForMember(dest => dest.ValueDto, opts => opts.MapFrom(src => src.Value));
+
+    //        }).CreateMapper();
     //    }
 
     //    [Fact]
@@ -33,11 +51,11 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
     //        dalMock.Setup(_ => _.GetAll()).Returns(_entities);
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
     //        var actionResult = controller.GetAll();
     //        var okResult = actionResult as OkObjectResult;
-    //        var model = okResult.Value as IEnumerable<TestEntity>;
+    //        var model = okResult.Value as IEnumerable<TestDto>;
 
     //        Assert.Equal(model.Count(), _entities.Count);
     //        dalMock.Verify(_ => _.GetAll(), Times.Once);
@@ -49,11 +67,11 @@ namespace LSG.GenericCrud.Tests.Controllers
     //        var id = _entities[0].Id;
     //        var dalMock = new Mock<Crud<TestEntity>>();
     //        dalMock.Setup(_ => _.GetById(id)).Returns(_entities[0]);
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
-            
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
+
     //        var actionResult = controller.GetById(id);
     //        var okResult = actionResult as OkObjectResult;
-    //        var model = okResult.Value as TestEntity;
+    //        var model = okResult.Value as TestDto;
 
     //        Assert.Equal(model.Id, id);
     //        dalMock.Verify(_ => _.GetById(It.IsAny<Guid>()), Times.Once);
@@ -64,10 +82,10 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
     //        dalMock.Setup(_ => _.GetById(It.IsAny<Guid>())).Throws(new EntityNotFoundException());
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
-            
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
+
     //        var actionResult = controller.GetById(Guid.NewGuid());
-            
+
     //        Assert.IsType(typeof(NotFoundResult), actionResult);
     //        dalMock.Verify(_ => _.GetById(It.IsAny<Guid>()), Times.Once);
     //    }
@@ -76,9 +94,9 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    public void Create_ReturnsCreatedEntity()
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
-    //        var actionResult = controller.Create(_entity);
+    //        var actionResult = controller.Create(_dto);
 
     //        Assert.IsType<OkObjectResult>(actionResult);
     //        dalMock.Verify(_ => _.Create(It.IsAny<TestEntity>()), Times.Once);
@@ -88,9 +106,9 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    public void Update_ReturnsModifiedEntity()
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
-    //        var actionResult = controller.Update(_entity.Id, _entity);
+    //        var actionResult = controller.Update(_dto.Id, _dto);
 
     //        Assert.IsType<OkResult>(actionResult);
     //        dalMock.Verify(_ => _.Update(It.IsAny<Guid>(), It.IsAny<TestEntity>()), Times.Once);
@@ -101,9 +119,9 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
     //        dalMock.Setup(_ => _.Update(It.IsAny<Guid>(), It.IsAny<TestEntity>())).Throws<EntityNotFoundException>();
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
-    //        var actionResult = controller.Update(_entity.Id, _entity);
+    //        var actionResult = controller.Update(_dto.Id, _dto);
 
     //        Assert.IsType(typeof(NotFoundResult), actionResult);
     //        dalMock.Verify(_ => _.Update(It.IsAny<Guid>(), It.IsAny<TestEntity>()), Times.Once);
@@ -113,9 +131,9 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    public void Delete_ReturnsOk()
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
-    //        var actionResult = controller.Delete(_entity.Id);
+    //        var actionResult = controller.Delete(_dto.Id);
 
     //        Assert.IsType(typeof(OkResult), actionResult);
     //        dalMock.Verify(_ => _.Delete(It.IsAny<Guid>()), Times.Once);
@@ -126,9 +144,9 @@ namespace LSG.GenericCrud.Tests.Controllers
     //    {
     //        var dalMock = new Mock<Crud<TestEntity>>();
     //        dalMock.Setup(_ => _.Delete(It.IsAny<Guid>())).Throws<EntityNotFoundException>();
-    //        var controller = new CrudControllerWithoutService<TestEntity>(dalMock.Object);
+    //        var controller = new CrudController<TestDto, TestEntity>(dalMock.Object, _mapper);
 
-    //        var actionResult = controller.Delete(_entity.Id);
+    //        var actionResult = controller.Delete(_dto.Id);
 
     //        Assert.IsType(typeof(NotFoundResult), actionResult);
     //        dalMock.Verify(_ => _.Delete(It.IsAny<Guid>()), Times.Once);
