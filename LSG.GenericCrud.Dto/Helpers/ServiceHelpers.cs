48552ec19bbff6dfc3e21d5090997477871ece56
lonesomegeek <lonesomegeek@outlook.com>
2017-11-29 12:19:39 -0500

    initializer helpers

---
+++ LSG.GenericCrud.Dto/Helpers/ServiceHelpers.cs
@@ -0,0 +1,21 @@
+﻿using LSG.GenericCrud.Dto.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LSG.GenericCrud.Dto.Helpers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceHelpers
+    {
+        /// <summary>
+        /// Adds the crud dto.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public static void AddCrudDto(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(CrudService<,>));
+            services.AddScoped(typeof(HistoricalCrudService<,>));
+        }
+    }
+}
