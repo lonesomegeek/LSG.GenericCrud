5a25ca57976a62c82081db501408b819bd174778
lonesomegeek <lonesomegeek@outlook.com>
2018-02-19 19:54:32 -0500

    working version ready for production

---
+++ LSG.GenericCrud.Samples/Sample.CrudOptions/Models/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LSG.GenericCrud.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sample.CrudOptions.Models
+{
+    public class MyContext : BaseDbContext, IDbContext
+    {
+        public MyContext(DbContextOptions options, IServiceProvider serviceProvider) : base(options, serviceProvider)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
