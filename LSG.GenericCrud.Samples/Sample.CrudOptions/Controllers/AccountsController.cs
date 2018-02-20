825a346e3e5b2699cbbb9165d048b87dfd156e0d
lonesomegeek <lonesomegeek@outlook.com>
2018-02-19 19:54:32 -0500

    working version ready for production

---
+++ LSG.GenericCrud.Samples/Sample.CrudOptions/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LSG.GenericCrud.Controllers;
+using LSG.GenericCrud.Extensions.Handlers;
+using LSG.GenericCrud.Services;
+using Microsoft.AspNetCore.Mvc;
+using Sample.CrudOptions.Models;
+
+namespace Sample.CrudOptions.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountsController : CrudController<Account>, ICrudAuthorizationOptions
+    {
+        public AccountsController(ICrudService<Account> service) : base(service)
+        {
+        }
+
+        public CrudAuthorizationOptions Options => new CrudAuthorizationOptionsBuilder()
+            .IsCreateAvailable(true)
+            .IsReadAvailable(true)
+            .IsUpdateAvailable(false)
+            .IsDeleteAvailable(false)
+            .IsCreateProtected(true) // will search for accounts.create scope
+            .IsReadProtected(false)
+            .Build();
+    }
+}
---
+++ LSG.GenericCrud.Samples/Sample.CrudOptions/Controllers/TokensController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace Sample.CrudOptions.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokensController : Controller
+    {
+        [Route("accessToken")]
+        [AllowAnonymous]
+        public IActionResult GetAccessToken()
+        {
+            var client = new RestClient("https://lonesomegeek.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"client_id\":\"vAnevpML1exaQxv6wwzrFaAJhHHUPBB8\",\"client_secret\":\"7ibb2fALHH5FwOhciL0u_r8cgLowRB8-e8L38VQRCEX9QFtPsubjwPocbd1v3okJ\",\"audience\":\"https://genericcrud.lonesomegeek.com\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return Ok(response.Content);
+        }
+    }
+}
