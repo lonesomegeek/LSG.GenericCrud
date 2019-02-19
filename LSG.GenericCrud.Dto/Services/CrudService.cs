6d43058d3e01061c6d70383f01b2ba7bcc768a27
lonesomegeek <lonesomegeek@outlook.com>
2019-02-18 19:23:57 -0500

    some switching in dto services

--- LSG.GenericCrud.Dto/Services/CrudService.cs
+++ LSG.GenericCrud.Dto/Services/CrudService.cs
@@ -87,4 +87,82 @@ namespace LSG.GenericCrud.Dto.Services
             return _mapper.Map<TDto>(updatedEntity);
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TDto">The type of the dto.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <seealso cref="LSG.GenericCrud.Services.CrudService{TEntity}" />
+    /// <seealso cref="LSG.GenericCrud.Services.ICrudService{TDto}" />
+    public class CrudService<TId, TDto, TEntity> :
+        ICrudService<TId, TDto>
+        where TDto : IEntity<TId>
+        where TEntity : class, IEntity<TId>, new()
+    {
+        private readonly ICrudService<TId, TEntity> _service;
+
+        /// <summary>
+        /// The mapper
+        /// </summary>
+        private readonly IMapper _mapper;
+
+        private readonly ICrudRepository _repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CrudService{TDto, TEntity}"/> class.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        /// <param name="mapper">The mapper.</param>
+        public CrudService(ICrudService<TId, TEntity> service, ICrudRepository repository, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+            _repository = repository;
+            AutoCommit = false;
+            _service.AutoCommit = false;
+        }
+
+        public bool AutoCommit { get; set; }
+
+        public virtual TDto Create(TDto dto) => CreateAsync(dto).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> CreateAsync(TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+            var createdEntity = await _service.CreateAsync(entity);
+            await _repository.SaveChangesAsync();
+            return _mapper.Map<TDto>(createdEntity);
+        }
+
+        public virtual TDto Delete(TId id) => DeleteAsync(id).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> DeleteAsync(TId id)
+        {
+            var deletedEntity = await _service.DeleteAsync(id);
+            await _repository.SaveChangesAsync();
+            return _mapper.Map<TDto>(deletedEntity);
+        }
+
+        public virtual IEnumerable<TDto> GetAll() => GetAllAsync().GetAwaiter().GetResult();
+
+        public virtual async Task<IEnumerable<TDto>> GetAllAsync()
+        {
+            var entities = await _service.GetAllAsync();
+            return entities.Select(_ => _mapper.Map<TDto>(_));
+        }
+
+        public virtual async Task<TDto> GetByIdAsync(TId id) => _mapper.Map<TDto>(await _service.GetByIdAsync(id));
+
+        public virtual TDto GetById(TId id) => GetByIdAsync(id).GetAwaiter().GetResult();
+
+        public virtual TDto Update(TId id, TDto dto) => UpdateAsync(id, dto).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> UpdateAsync(TId id, TDto dto)
+        {
+            var updatedEntity = await _service.UpdateAsync(id, _mapper.Map<TEntity>(dto));
+            await _repository.SaveChangesAsync();
+            return _mapper.Map<TDto>(updatedEntity);
+        }
+    }
 }
--- LSG.GenericCrud.Dto/Services/HistoricalCrudService.cs
+++ LSG.GenericCrud.Dto/Services/HistoricalCrudService.cs
@@ -85,4 +85,81 @@ namespace LSG.GenericCrud.Dto.Services
             return _mapper.Map<TDto>(updatedEntity);
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TDto">The type of the dto.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <seealso cref="LSG.GenericCrud.Services.HistoricalCrudService{TEntity}" />
+    /// <seealso cref="LSG.GenericCrud.Services.IHistoricalCrudService{TDto}" />
+    public class HistoricalCrudService<TId, TDto, TEntity> :
+        ICrudService<TId, TDto>,
+        IHistoricalCrudService<TId, TDto>
+        where TDto : IEntity<TId>
+        where TEntity : class, IEntity<TId>, new()
+    {
+        private readonly IHistoricalCrudService<TId, TEntity> _service;
+        private readonly ICrudRepository _repository;
+        private readonly IMapper _mapper;
+
+        public HistoricalCrudService(IHistoricalCrudService<TId, TEntity> service, ICrudRepository repository, IMapper mapper)
+        {
+            _service = service;
+            _repository = repository;
+            _mapper = mapper;
+            AutoCommit = false;
+        }
+
+        public bool AutoCommit { get; set; }
+
+        public virtual TDto Create(TDto dto) => CreateAsync(dto).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> CreateAsync(TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+            var createdEntity = await _service.CreateAsync(entity);
+            return _mapper.Map<TDto>(createdEntity);
+        }
+
+        public virtual TDto Delete(TId id) => DeleteAsync(id).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> DeleteAsync(TId id)
+        {
+            var deletedEntity = await _service.DeleteAsync(id);
+            return _mapper.Map<TDto>(deletedEntity);
+        }
+
+        public virtual IEnumerable<TDto> GetAll() => GetAllAsync().GetAwaiter().GetResult();
+
+        public virtual async Task<IEnumerable<TDto>> GetAllAsync()
+        {
+            var entities = await _service.GetAllAsync();
+            return entities.Select(_ => _mapper.Map<TDto>(_));
+        }
+
+        public virtual async Task<TDto> GetByIdAsync(TId id) => _mapper.Map<TDto>(await _service.GetByIdAsync(id));
+
+        public virtual TDto GetById(TId id) => GetByIdAsync(id).GetAwaiter().GetResult();
+
+        public virtual IEnumerable<IEntity> GetHistory(TId id) => GetHistoryAsync(id).GetAwaiter().GetResult();
+
+        public virtual async Task<IEnumerable<IEntity>> GetHistoryAsync(TId id) => await _service.GetHistoryAsync(id);
+
+        public virtual TDto Restore(TId id) => RestoreAsync(id).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> RestoreAsync(TId id)
+        {
+            var restoredEntity = await _service.RestoreAsync(id);
+            return _mapper.Map<TDto>(restoredEntity);
+        }
+
+        public virtual TDto Update(TId id, TDto dto) => UpdateAsync(id, dto).GetAwaiter().GetResult();
+
+        public virtual async Task<TDto> UpdateAsync(TId id, TDto dto)
+        {
+            var updatedEntity = await _service.UpdateAsync(id, _mapper.Map<TEntity>(dto));
+            return _mapper.Map<TDto>(updatedEntity);
+        }
+    }
 }
