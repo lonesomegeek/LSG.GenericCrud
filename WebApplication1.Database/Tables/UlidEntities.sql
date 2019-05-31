37109cee0a2775941f5bbdc4303e222b376f1201
lonesomegeek <lonesomegeek@outlook.com>
2019-05-30 20:42:15 -0400

    removing useless files for library

--- WebApplication1.Database/Tables/UlidEntities.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[UlidEntities] (
-    [Id]           nvarchar(50) NOT NULL,
-    [Value]         NVARCHAR (50)    NULL
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- WebApplication1.Database/Tables/Users.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [Id]   UNIQUEIDENTIFIER NOT NULL,
-    [Name] NVARCHAR (50)    NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
