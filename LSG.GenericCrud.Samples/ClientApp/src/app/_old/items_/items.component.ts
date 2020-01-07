879289e77735f5ea1af5ebc4c235573b96a78c76
lonesomegeek <lonesomegeek@outlook.com>
2020-01-07 12:46:14 -0500

    adding new samples structure

---
+++ LSG.GenericCrud.Samples/ClientApp/src/app/_old/items_/items.component.ts
@@ -0,0 +1,17 @@
+import { CrudDataComponent } from "../_generics_/crud.component";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "crud-item-component",
+  templateUrl: "../_generics_/crud.component.html"
+})
+export class CrudItemComponent extends CrudDataComponent<Item> {
+  columnDefs: any[] = [
+    { headerName: 'Id', field: 'id', sortable: true },
+    { headerName: 'Name', field: 'name', sortable: true }];  
+  baseRoute: string = "api/items";
+}
+
+export interface Item {
+  name: string;
+}
---
+++ LSG.GenericCrud.Samples/Controllers/MostRecentlyUsedController.cs
@@ -0,0 +1,47 @@
+﻿using LSG.GenericCrud.Models;
+using LSG.GenericCrud.Repositories;
+using LSG.GenericCrud.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.App.Controllers
+{
+    public class MostRecentlyUsedController : ControllerBase
+    {
+        private readonly ICrudRepository _repository;
+        private readonly IUserInfoRepository _userInfoReposiory;
+
+        public MostRecentlyUsedController(
+            ICrudRepository repository,
+            IUserInfoRepository userInfoReposiory)
+        {
+            _repository = repository;
+            _userInfoReposiory = userInfoReposiory;
+        }
+
+        [HttpGet("most-recently-used")]
+        public async Task<IActionResult> GetMostRecenlyUsed()
+        {
+
+            var events = _repository
+                .GetAllAsync<Guid, HistoricalEvent>().Result
+                .Where(_ =>
+                    _.Action == HistoricalActions.Read.ToString() &&
+                    _.CreatedBy == _userInfoReposiory.GetUserInfo())
+                .OrderByDescending(_ => _.CreatedDate)
+                .GroupBy(_ => _.EntityId)
+                .Select(_ => new
+                {                    
+                    EntityId = _.Key,                    
+                    LastEvent = _.OrderByDescending(e => e.CreatedDate).FirstOrDefault()
+
+                });
+                //.Take(10);
+
+            return Ok(events);
+        }
+    }
+}
