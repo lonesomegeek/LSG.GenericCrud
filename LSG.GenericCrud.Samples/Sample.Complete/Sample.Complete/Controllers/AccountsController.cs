a0d44226eadc2c7434033da648251b2e3a174bdb
Emmanuel Dugas-Gallant - DUGE <DUGE@premiertech.com>
2020-01-08 09:19:22 -0500

    demo done

--- LSG.GenericCrud.Samples/Sample.Complete/Sample.Complete/Controllers/AccountsController.cs
+++ LSG.GenericCrud.Samples/Sample.Complete/Sample.Complete/Controllers/AccountsController.cs
@@ -1,27 +1,54 @@
-﻿using System;
-using System.Linq;
-using LSG.GenericCrud.Controllers;
-using Microsoft.AspNetCore.Mvc;
-using Sample.Complete.Models.Entities;
+﻿using LSG.GenericCrud.Controllers;
+using LSG.GenericCrud.Repositories;
 using LSG.GenericCrud.Services;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Complete.Models.DTOs;
+using Sample.Complete.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Sample.Complete.Controllers
 {
     [Route("api/[controller]")]
-    public class AccountsController : CrudController<Guid, Account>
+    [ApiController]
+    public class AccountsController :
+        ControllerBase,
+        ICrudController<Guid, AccountDto>,
+        IHistoricalCrudController<Guid, AccountDto>
     {
-        private readonly ICrudService<Guid, Contact> _contactService;
+        private readonly IHistoricalCrudController<Guid, AccountDto> _controller;
+        private readonly IHistoricalCrudRestoreController<Guid, AccountDto> _restoreController;
+        private readonly IHistoricalCrudService<Guid, AccountDto> _service;
 
-        public AccountsController(ICrudService<Guid, Account> service, ICrudService<Guid, Contact> contactService) : base(service)
+        public AccountsController(
+            IHistoricalCrudController<Guid, AccountDto> controller,
+            IHistoricalCrudRestoreController<Guid, AccountDto> restoreController,
+            IHistoricalCrudService<Guid, AccountDto> service)
         {
-            _contactService = contactService;
+            _controller = controller;
+            _restoreController = restoreController;
+
+            _service = service;
         }
 
-        [Route("{accountId}/contacts")]
-        public IActionResult GetAccountContacts(Guid accountId)
-        {
-            return Ok(_contactService.GetAll().Where(_ => _.AccountId == accountId));
-        }
-
+        [HttpHead("{id}")]
+        public async Task<IActionResult> HeadById(Guid id) => await _controller.HeadById(id);
+        [HttpPost]
+        public async Task<ActionResult<AccountDto>> Create([FromBody] AccountDto dto) => await _controller.Create(dto);
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AccountDto>> Delete(Guid id) => await _controller.Delete(id);
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountDto>>> GetAll() => await _controller.GetAll();
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<ActionResult<AccountDto>> GetById(Guid id) => await _controller.GetById(id);
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] AccountDto dto) => await _controller.Update(id, dto);
+        [HttpGet("{id}/history")]
+        public async Task<IActionResult> GetHistory(Guid id) => await _controller.GetHistory(id);
+        [HttpPost("{id}/restore")]
+        public async Task<IActionResult> Restore(Guid id) => await _restoreController.RestoreFromDeletedEntity(id);
     }
 }
--- LSG.GenericCrud.Samples/Sample.Complete/Sample.Complete/Startup.cs
+++ LSG.GenericCrud.Samples/Sample.Complete/Sample.Complete/Startup.cs
@@ -47,7 +47,7 @@ namespace Sample.Complete
 
             // Specifies to use this context with an InMemory Database connection
             //services.AddDbContext<MyContext>(opt => opt.UseInMemoryDatabase());
-            services.AddDbContext<MyContext>(opt => opt.UseSqlServer("server=(localdb)\\mssqllocaldb;Initial Catalog=Sample.Complete"));
+            services.AddDbContext<MyContext>(opt => opt.UseSqlServer("server=localhost;Initial Catalog=LSG.GenericCrud.Sample.Complete;Integrated Security=True;"));
 
             // Map our dynamic repository to our custom context
             services.AddTransient<IDbContext, MyContext>();
