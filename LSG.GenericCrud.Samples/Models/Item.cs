b618975a9978292b03ae9f2a9f8b723eae663633
lonesomegeek <lonesomegeek@outlook.com>
2020-01-22 12:58:04 -0500

    adding material to test IEntity vs BaseEntity

---
+++ LSG.GenericCrud.Samples/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSG.GenericCrud.Samples.Models
+{
+    public class Item
+    {
+    }
+}
