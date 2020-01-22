0a8fbae07935b609ea6876295c2c2ea629cb037e
lonesomegeek <lonesomegeek@outlook.com>
2020-01-21 21:50:46 -0500

    adding benchmark files

---
+++ LSG.GenericCrud.Benchmark/Models/BenchmarkingContext.cs
@@ -0,0 +1,22 @@
+﻿using LSG.GenericCrud.Models;
+using LSG.GenericCrud.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSG.GenericCrud.Benchmark.Models
+{
+    public class BenchmarkingContext :
+        BaseDbContext,
+        IDbContext
+    {
+        public BenchmarkingContext(DbContextOptions options, IServiceProvider serviceProvider) : base(options, serviceProvider)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<HistoricalEvent> Events { get; set; }
+        public DbSet<HistoricalChangeset> Changesets { get; set; }
+    }
+}
