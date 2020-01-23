6721bf8cbedf6544e2e778a5345a601ec828e420
lonesomegeek <lonesomegeek@outlook.com>
2020-01-22 21:56:03 -0500

    putting in on track

---
+++ LSG.GenericCrud.Samples/Models/SampleContext.cs
@@ -0,0 +1,22 @@
+﻿using LSG.GenericCrud.Models;
+using LSG.GenericCrud.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSG.GenericCrud.Samples.Models
+{
+    public class SampleContext :
+        BaseDbContext,
+        IDbContext
+    {
+        public SampleContext(DbContextOptions options, IServiceProvider serviceProvider) : base(options, serviceProvider)
+        {
+        }
+        public DbSet<HistoricalChangeset> HistoricalChangesets { get; set; }
+        public DbSet<HistoricalEvent> HistoricalEvents { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
