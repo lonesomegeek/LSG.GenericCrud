eadb72b5ec68dce1df624f920d28ac3a6c168351
lonesomegeek <lonesomegeek@outlook.com>
2020-02-29 10:50:58 -0500

    working odata project with rest api without odata for now

---
+++ LSG.GenericCrud.Samples.OData/Controllers/AccountsController.cs
@@ -0,0 +1,24 @@
+﻿using LSG.GenericCrud.Samples.OData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSG.GenericCrud.Samples.OData.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AccountsController : ControllerBase
+    {
+        public IEnumerable<Account> GetAll()
+        {
+            return new List<Account>
+            {
+                new Account { Id = Guid.NewGuid(), Name = "Test1" },
+                new Account { Id = Guid.NewGuid(), Name = "Test2" },
+            };
+        }
+    }
+}
---
+++ LSG.GenericCrud.Samples.OData/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSG.GenericCrud.Samples.OData.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
