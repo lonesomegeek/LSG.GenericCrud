fbf60389a5e435831c64570e42ec6a4fb5765d4b
lonesomegeek <lonesomegeek@outlook.com>
2020-03-01 13:56:48 -0500

    working controller with odata and aspnetcore

--- LSG.GenericCrud.Samples.OData/Controllers/AccountsController.cs
+++ LSG.GenericCrud.Samples.OData/Controllers/AccountsController.cs
@@ -11,7 +11,7 @@ namespace LSG.GenericCrud.Samples.OData.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountsController : 
+    public class AccountsController :
         ControllerBase,
         ICrudController<Guid, Account>
     {
@@ -21,55 +21,89 @@ namespace LSG.GenericCrud.Samples.OData.Controllers
         {
             _controller = controller;
         }
-        public Task<ActionResult<Account>> Create([FromBody] Account entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<ActionResult<Account>> Delete(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpPost]
+        public async Task<ActionResult<Account>> Create([FromBody] Account entity) => await _controller.Create(entity);
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Account>> Delete(Guid id) => await _controller.Delete(id);
+
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAll() => await _controller.GetAll();
 
         [HttpGet]
         [EnableQuery()]
-        public async Task<ActionResult<IEnumerable<Account>>> Get()
-        {
-            return await _controller.GetAll();
-            return new List<Account>
-            {
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f01"), Name = "Test1" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f02"), Name = "Test2" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f03"), Name = "Test3" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f04"), Name = "Test4" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f05"), Name = "Test5" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f06"), Name = "Test6" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f07"), Name = "Test7" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f08"), Name = "Test8" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f09"), Name = "Test9" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f10"), Name = "Test10" },
-                new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f11"), Name = "Test11" },
-            };
-        }
+        public async Task<ActionResult<IEnumerable<Account>>> Get() => await _controller.GetAll();
 
-        public Task<ActionResult<IEnumerable<Account>>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetById(Guid id) => await _controller.GetById(id);
 
-        public Task<ActionResult<Account>> GetById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpHead("{id}")]
+        public async Task<IActionResult> HeadById(Guid id) => await _controller.HeadById(id);
 
-        public Task<IActionResult> HeadById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IActionResult> Update(Guid id, [FromBody] Account entity)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] Account entity) => await _controller.Update(id, entity);
     }
+
+    //public class AccountsController : 
+    //    ControllerBase,
+    //    ICrudController<Guid, Account>
+    //{
+    //    private readonly ICrudController<Guid, Account> _controller;
+
+    //    public AccountsController(ICrudController<Guid, Account> controller)
+    //    {
+    //        _controller = controller;
+    //    }
+    //    public Task<ActionResult<Account>> Create([FromBody] Account entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<ActionResult<Account>> Delete(Guid id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    [HttpGet]
+    //    [EnableQuery()]
+    //    public async Task<ActionResult<IEnumerable<Account>>> Get()
+    //    {
+    //        return await _controller.GetAll();
+    //        return new List<Account>
+    //        {
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f01"), Name = "Test1" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f02"), Name = "Test2" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f03"), Name = "Test3" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f04"), Name = "Test4" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f05"), Name = "Test5" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f06"), Name = "Test6" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f07"), Name = "Test7" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f08"), Name = "Test8" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f09"), Name = "Test9" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f10"), Name = "Test10" },
+    //            new Account { Id = Guid.Parse("d74f455c-eb06-4997-8362-01ad89980f11"), Name = "Test11" },
+    //        };
+    //    }
+
+    //    public Task<ActionResult<IEnumerable<Account>>> GetAll()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<ActionResult<Account>> GetById(Guid id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<IActionResult> HeadById(Guid id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<IActionResult> Update(Guid id, [FromBody] Account entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
