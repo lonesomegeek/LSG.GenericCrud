2d32cebca77e7374dcf008c2771c9a9277b02f2b
lonesomegeek <lonesomegeek@outlook.com>
2020-04-05 14:42:31 -0400

    remove item controller, new tables

--- LSG.GenericCrud.Samples/Databases/Scripts/Tables.sql
+++ LSG.GenericCrud.Samples/Databases/Scripts/Tables.sql
@@ -157,3 +157,15 @@ CREATE TABLE [dbo].[Shares](
 			ALLOW_ROW_LOCKS = ON,
 			ALLOW_PAGE_LOCKS = ON
 		) ON [PRIMARY])
+
+
+CREATE TABLE [dbo].[Users](
+		[Id] [uniqueidentifier] NOT NULL,
+		[Name] nvarchar(50) NOT NULL,
+		PRIMARY KEY CLUSTERED ([Id] ASC) WITH (
+			PAD_INDEX = OFF,
+			STATISTICS_NORECOMPUTE = OFF,
+			IGNORE_DUP_KEY = OFF,
+			ALLOW_ROW_LOCKS = ON,
+			ALLOW_PAGE_LOCKS = ON
+		) ON [PRIMARY])
--- LSG.GenericCrud.Samples/Models/SampleContext.cs
+++ LSG.GenericCrud.Samples/Models/SampleContext.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using LSG.GenericCrud.Samples.Models.Entities;
 using Object = LSG.GenericCrud.Samples.Models.Entities.Object;
+using User = LSG.GenericCrud.Samples.Models.Entities.User;
 
 namespace LSG.GenericCrud.Samples.Models
 {
@@ -17,10 +18,11 @@ namespace LSG.GenericCrud.Samples.Models
         }
         public DbSet<HistoricalChangeset> HistoricalChangesets { get; set; }
         public DbSet<HistoricalEvent> HistoricalEvents { get; set; }
-        public DbSet<Item> Items { get; set; }
+        // public DbSet<Item> Items { get; set; }
         public DbSet<Object> Objects { get; set; }
         public DbSet<Share> Shares { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<User> Users { get;set; }
 
     }
 }
