2478231bfd9411cd721e04668ce4873e242a3275
lonesomegeek <lonesomegeek@outlook.com>
2020-04-19 11:45:44 -0400

    adding sample database project

---
+++ LSG.GenericCrud.Samples.Database/Tables/Shares.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Shares]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	[ContactId] [uniqueidentifier] NOT NULL,
+	[ItemId] [uniqueidentifier] NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[SharingReminder] datetime2 NULL
+
+	-- constraints
+	CONSTRAINT PK_Shares PRIMARY KEY (Id)
+)
